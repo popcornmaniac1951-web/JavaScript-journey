05d62dd3fedb5c83583e3959b607f9db7305fc85
popcornmaniac1951-web <popcornmaniac1951@gmail.com>
2026-02-02 20:54:54 +0530

    start collecting all javascript methods to practice functions

---
+++ Functions/Exercises/introduction.js
@@ -0,0 +1,12 @@
+//1.1 length of a string
+
+const abc = 'hello';
+console.log(abc.length);
+
+//1.2.bracket notation
+
+console.log(abc[0]);//it will print h .first character of the string
+console.log(abc[abc.length - 1]);// it will print the last character in the string
+
+//Exercise or Examples
+

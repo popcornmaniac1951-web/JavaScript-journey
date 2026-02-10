9e6f70cf0071cac33164f5ba96d37536874f2d17
popcornmaniac1951-web <popcornmaniac1951@gmail.com>
2026-02-10 20:45:07 +0530

    Array Spliting,Reversing,Joining

---
+++ Array/WorkingWithArray/Array-split-reverse-join.js
@@ -0,0 +1,18 @@
+console.log('String spliting into array using the split method');
+
+let abc = 'hello';
+let abd = abc.split("");
+console.log(abd);
+
+
+console.log("Joining the splited array into the joined array");
+let acd = abd.join("");
+console.log(acd);
+
+console.log('Reverse of a array');
+
+
+let dfd = ['h','e','l','l','o'];
+let dfm = dfd.reverse();
+console.log(dfm);
+console.log(dfm.join(""));

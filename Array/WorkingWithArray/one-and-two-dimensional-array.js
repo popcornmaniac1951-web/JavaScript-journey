fc803c80f69913f1a25d74c76c2b741cddb19340
popcornmaniac1951-web <popcornmaniac1951@gmail.com>
2026-02-10 15:47:04 +0530

    one dimensional array and Two dimensional array

---
+++ Array/WorkingWithArray/one-and-two-dimensional-array.js
@@ -0,0 +1,15 @@
+//1. One-dimensional Array
+
+let fruits = ['apple','orange','mango'];
+console.log(fruits[0]);
+
+//2. Two-dimensional Array
+
+let vehicles = [
+    ['1','2','3','4'],
+    ['a','b','c','d'],
+    ['apple','orange','codex','he-man']
+];
+
+console.log(vehicles[0][2]);
+console.log(vehicles[1][1]);

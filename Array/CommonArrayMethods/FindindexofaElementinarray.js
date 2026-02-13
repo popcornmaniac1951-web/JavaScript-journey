34a1b1f69ce19645a865c2426fe21f9b21faac6b
popcornmaniac1951-web <popcornmaniac1951@gmail.com>
2026-02-13 16:02:52 +0530

    find index of a element in array

---
+++ Array/CommonArrayMethods/FindindexofaElementinarray.js
@@ -0,0 +1,11 @@
+let colors = ["red", "green", "blue", "yellow", "green"];
+let w = colors.indexOf("green", 3);
+console.log(w); // 4
+
+let fruits = ["apple", "banana", "orange"];
+let index = fruits.indexOf("grape");
+console.log(index); // -1
+
+let numbers = [10, 20, 30, 20, 40];
+let s = numbers.indexOf(20);
+console.log(s);

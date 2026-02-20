247c393bd4e02d1d1e482c2c364699bca893d0c2
popcornmaniac1951-web <popcornmaniac1951@gmail.com>
2026-02-20 16:47:57 +0530

    how  the for of loop works and where to use it

---
+++ Loops/working-with-loops/how-should-for-in-loop-works-when-to-use-it.js
@@ -0,0 +1,9 @@
+const prp = {
+    name : 'athul',
+    age : 23,
+    place : 'angamaly'
+}
+
+for(prop in prp){
+    console.log()
+}
---
+++ Loops/working-with-loops/how-should-for-of-loop-works-when-to-use-it.js
@@ -0,0 +1,15 @@
+
+console.log('for of loop in array');
+const arr = [1,2,3,4,5];
+
+for(num of arr){
+    console.log(num);
+}
+
+
+console.log('for of loop in string');
+const srt = 'freecodecamp';
+for(srk of srt){
+    console.log(srk);
+}
+

c7e0c982fe3adc053f8aef8a5328e76ef380ff9d
popcornmaniac1951-web <popcornmaniac1951@gmail.com>
2026-02-20 11:37:13 +0530

    Loops started . How loops and iteration works

---
+++ Loops/working-with-loops/how-loops-and-iteration-works.js
@@ -0,0 +1,17 @@
+console.log('demo of a loop ');
+for(let i = 0; i<5 ; i++){
+    console.log(i);
+}
+
+console.log('print numbers from 10 to 1');
+
+for(let i = 10 ; i>0 ;i--){
+    console.log(i);
+}
+
+console.log('print even numbers from 1 to 20');
+
+for(let i = 2 ; i<=20; i += 2){
+    console.log(i);
+}
+

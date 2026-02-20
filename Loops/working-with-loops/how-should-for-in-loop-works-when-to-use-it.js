2ae7e6a34945519399e15c48fc3d1f2606573998
popcornmaniac1951-web <popcornmaniac1951@gmail.com>
2026-02-20 21:19:12 +0530

    How should for in loop works and when to use it

--- Loops/working-with-loops/how-should-for-in-loop-works-when-to-use-it.js
+++ Loops/working-with-loops/how-should-for-in-loop-works-when-to-use-it.js
@@ -1,3 +1,4 @@
+console.log('for in loop and how it works');
 const prp = {
     name : 'athul',
     age : 23,
@@ -5,5 +6,53 @@ const prp = {
 }
 
 for(prop in prp){
-    console.log()
+    console.log(prp[prop]);
+}
+
+const fruit = {
+  name: 'apple',
+  color: 'red',
+  price: 0.99
+};
+
+for (const prop in fruit) {
+  console.log(fruit[prop]);
+}
+
+const persond = {
+  name: 'John',
+  age: 30,
+  address: {
+    street: '123 Main St',
+    city: 'Anytown',
+    state: 'CA'
+  }
+};
+
+for (const prop in person) {
+  console.log(persond[prop]);
+}
+
+const person = {
+  name: 'John',
+  age: 30,
+  address: {
+    street: '123 Main St',
+    city: 'Anytown',
+    state: 'CA'
+  }
+};
+
+function isObject(obj) {
+  return typeof obj === 'object' && !Array.isArray(obj) && obj !== null;
+}
+
+for (const prop in person) {
+  if (isObject(person[prop])) {
+    for (const nestedProp in person[prop]) {
+      console.log(person[prop][nestedProp]);
+    }
+  } else {
+    console.log(person[prop]);
+  }
 }

e7d08f12ca6c52c2d703b608160e74ec08e5e683
popcornmaniac1951-web <popcornmaniac1951@gmail.com>
2026-03-29 15:57:54 +0530

    What is While Loop and Do While Loop

---
+++ Loops/working-with-loops/while-loop-and-do-while-loop.js
@@ -0,0 +1,19 @@
+// While Loop
+// First check the condition and then move to the block of code.
+
+let counter = 0;
+while(counter < 5)
+{
+    console.log(counter);
+    counter ++;
+}
+
+// Do While Loop
+// First execute the block of code atleast once and then check the condition if it true it will execute and it is false it will not execute.
+
+let counters = 0;
+do{
+     console.log(counter);
+    counter ++;
+}
+while(counter < 5);
